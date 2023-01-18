f57b9dde319613c49fb6bd76b5a9e6cc9ec15ccd
Kwanele Gcaleka <kwanelekway0101@gmail.com>
2023-01-18 17:55:43 +0200

    Saving changes to App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
 
 function App(){
   return (
     <BrowserRouter>
       <div className="App">
         <Header />
+        <Routes>
+          <Route path="/" element={ <Home /> } exact />
+          <Route path="/about" element={ <About /> } />
+          <Route path="/projects" element={ <Projects /> } />
+        </Routes>
         <Footer />
       </div>
     </BrowserRouter>
